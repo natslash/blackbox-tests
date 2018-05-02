1449a3f6d80e3fdc9c9cae123ecd3edee6e4d8c5
Shashi Subramanyam <shashi.subramanyam@orenesgrupo.com>
2018-05-02 16:01:18 +0200

    Added REST Service test example

---
+++ src/test/java/es/talos/demo/it/RESTServiceTestIT.java
@@ -0,0 +1,17 @@
+package es.talos.demo.it;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.CoreMatchers.hasItems;
+
+import org.testng.annotations.Test;
+
+public class RESTServiceTestIT {
+	@Test
+	public void givenUrl_whenJsonResponseHasArrayWithGivenValuesUnderKey_thenCorrect() {
+		when().
+			get("https://reqres.in/api/users").
+		then().statusCode(200).
+		assertThat().body("data.last_name",
+				hasItems("Weaver"));
+	}
+}
