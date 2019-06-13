ef7ca9048bc6e480806e57aae551b5ef627403e7
Shashidhara Subramanyam <Shashidhara.Subramanyam@axoom.com>
2019-06-13 14:02:18 +0200

    Added new methods to subjects client

--- src/main/java/axoom/subjects/v1/SubjectzClient.java
+++ src/main/java/axoom/subjects/v1/SubjectzClient.java
@@ -7,6 +7,8 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import com.axoom.constants.ContentType;
 import com.axoom.drs.utils.RestUtils;
+import axoom.filters.v1.Filter.KeyValueFilter;
+import axoom.filters.v1.Filter.StringPropertyFilter;
 import axoom.subjects.v1.Subjects.Subject;
 import axoom.subjects.v1.Subjects.SubjectContext;
 import axoom.subjects.v1.Subjects.SubjectType;
@@ -18,6 +20,12 @@ import axoom.subjects.v1.SubjectsService.GetSubjectContextRequest;
 import axoom.subjects.v1.SubjectsService.GetSubjectRequest;
 import axoom.subjects.v1.SubjectsService.GetSubjectTypeContextRequest;
 import axoom.subjects.v1.SubjectsService.GetSubjectTypeRequest;
+import axoom.subjects.v1.SubjectsService.ListSubjectTypesRequest;
+import axoom.subjects.v1.SubjectsService.ListSubjectTypesResponse;
+import axoom.subjects.v1.SubjectsService.ListSubjectsRequest;
+import axoom.subjects.v1.SubjectsService.ListSubjectsResponse;
+import axoom.subjects.v1.SubjectsService.SubjectFilter;
+import axoom.subjects.v1.SubjectsService.SubjectTypeFilter;
 import axoom.subjects.v1.SubjectsService.UpdateSubjectRequest;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
@@ -166,6 +174,38 @@ public class SubjectzClient {
 
     try {      
       return blockingStub.getSubjectTypeContext(request);
+    } catch (StatusRuntimeException sre) {
+      logger.log(Level.SEVERE, "RPC failed: {0}", sre.getStatus());
+      throw sre;
+    }
+  }
+  
+  /**
+   * 
+   * @param subjectName
+   * @return listSubjectsResponse
+   */
+  public ListSubjectsResponse listSubjects(String subjectName) {
+    StringPropertyFilter filter = StringPropertyFilter.newBuilder().setValue(subjectName).build();
+    SubjectFilter subjectFilter = SubjectFilter.newBuilder().setName(filter).build();
+    ListSubjectsRequest request = ListSubjectsRequest.newBuilder().setFilter(subjectFilter).build();
+
+    try {      
+      return blockingStub.listSubjects(request);
+    } catch (StatusRuntimeException sre) {
+      logger.log(Level.SEVERE, "RPC failed: {0}", sre.getStatus());
+      throw sre;
+    }
+  }
+  
+  
+  public ListSubjectTypesResponse listSubjectTypes(String subjectTypeName) {
+    KeyValueFilter filter = KeyValueFilter.newBuilder().setKey("label").setValue(subjectTypeName).build();
+    SubjectTypeFilter subjectFilter = SubjectTypeFilter.newBuilder().setLabels(0, filter).build();
+    ListSubjectTypesRequest request = ListSubjectTypesRequest.newBuilder().setFilter(subjectFilter).build();
+
+    try {      
+      return blockingStub.listSubjectTypes(request);
     } catch (StatusRuntimeException sre) {
       logger.log(Level.SEVERE, "RPC failed: {0}", sre.getStatus());
       throw sre;
